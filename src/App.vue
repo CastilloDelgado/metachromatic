a4def636129fa3a9442eb9dc8d1ecfacf1c99668
Marco Castillo <marcocastillo1997@gmail.com>
2024-05-22 21:57:42 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-black w-full h-screen grid place-content-center">
+    <a href="https://www.instagram.com/metachromatic.inc/">
+      <p class="text-white text-2xl uppercase italic font-serif">Metachromatic</p>
+    </a>
+  </div>
+</template>
