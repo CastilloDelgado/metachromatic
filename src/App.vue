15989afea3bc7f4ac81b8eb3abae2136226d5466
Marco Castillo <marcocastillo1997@gmail.com>
2024-06-19 21:25:32 -0600

    Updating metachromatic site

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="bg-black w-full h-screen grid place-content-center">
-    <a href="https://www.instagram.com/metachromatic.inc/">
-      <p class="text-white text-2xl uppercase italic font-serif">Metachromatic</p>
-    </a>
+  <div class="w-screen h-screen  grid place-content-center background">
+    <div class="w-screen grid place-content-center py-8 mx-auto bg-black">
+      <p class="font-serif text-center uppercase text-2xl md:text-4xl font-bold text-white">Metachromatic</p>
+      <div class="flex justify-between space-x-1 text-sm">
+        <a class="text-neutral-400 hover:text-white capitalize font-sans font-light  hover:underline"
+          href="https://www.instagram.com/metachromatic.inc/">Instagram</a>
+        <a class="text-neutral-400 hover:text-white capitalize font-sans font-light  hover:underline"
+          href="https://soundcloud.com/metachromatic">Soundcloud</a>
+        <a class="text-neutral-400 hover:text-white capitalize font-sans font-light  hover:underline"
+          href="https://www.youtube.com/@metachromatic_">YouTube</a>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.background {
+  background-image: url('./assets/logo.png');
+  background-size: 200px;
+}
+</style>
